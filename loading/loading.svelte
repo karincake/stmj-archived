1ca200212650fab8f4d23463ea2f3d98f6afc727
Munawwirul Jamal <munawwirul.jamal@gmail.com>
2023-08-04 18:20:06 +0700

    dev: added loading

---
+++ loading/loading.svelte
@@ -0,0 +1,7 @@
+<div class="d-flex flex-column py-3">
+	<div class="d-flex justify-content-center mb-3">
+		<div class="spinner-border" role="status">
+	</div>
+	</div>
+	<div class="text-center">Loading Data</div>
+</div>
